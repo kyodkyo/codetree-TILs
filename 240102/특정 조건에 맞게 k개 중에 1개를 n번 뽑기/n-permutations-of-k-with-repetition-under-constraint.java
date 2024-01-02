fabe8bdf06d18d17d57c65507fa3f7bca2eef1cc
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-01-02 21:21:21 +0900

    Title: 특정 조건에 맞게 k개 중에 1개를 n번 뽑기, Time: 1406ms, Memory: 35MB, Status: Passed - CodeTree

--- 240102/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
+++ 240102/특정 조건에 맞게 k개 중에 1개를 n번 뽑기/n-permutations-of-k-with-repetition-under-constraint.java
@@ -22,27 +22,15 @@ public class Main {
         }
 
         for(int i=1; i<=k; i++){
-            if (num>=3){
-                if (!isPossible(num-1, i))
-                    list.add(i);
-                else
-                    continue;
-            }
-            else
-                list.add(i);
+            if (num>=3 && list.get(num-2)==i && list.get(num-3)==i)
+                continue;
+            list.add(i);
             selectNum(num+1);
             list.remove(list.size()-1);
         }
 
     }
-    public static boolean isPossible(int pos, int num){
-        boolean check = false;
 
-        if (list.get(pos-1) == num && list.get(pos-2)==num)
-            check = true;
-
-        return check;
-    }
     public static void printNums(){
         for(int i=0; i<list.size(); i++)
             System.out.print(list.get(i) + " ");
