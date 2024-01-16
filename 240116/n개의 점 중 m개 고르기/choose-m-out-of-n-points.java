cbe8047dfc560d75977f8cca31c51487b0fb9206
dakyo Kang <104312241+kyodkyo@users.noreply.github.com>
2024-01-16 18:30:57 +0900

    Title: n개의 점 중 m개 고르기, Time: 210ms, Memory: 11MB, Status: Passed - CodeTree

--- 240116/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
+++ 240116/n개의 점 중 m개 고르기/choose-m-out-of-n-points.java
@@ -48,7 +48,7 @@ public class Main {
         for(int i=0; i<list.size()-1; i++){
             for(int j=i+1; j<list.size(); j++){
                 int dx = Math.abs(list.get(i).x - list.get(j).x);
-                int dy = Math.abs(list.get(i).x - list.get(j).y);
+                int dy = Math.abs(list.get(i).y - list.get(j).y);
                 max = Math.max(max, (int)(Math.pow(dx, 2) + Math.pow(dy, 2)));
             }
         }
